ff3bfde2a6fec5bdcd6866150fe0b794cca882db
aydosjs <aji3385@gmail.com>
2023-09-21 22:32:59 +0500

    tets

---
+++ bot/bot.js
@@ -0,0 +1,7 @@
+import { Telegraf } from "telegraf";
+const TOKEN = `6422144117:AAFyAMiQym1MR2TBVBhJkobbCpZwOwlQqHQ`;
+const bot = new Telegraf(TOKEN);
+
+bot.start((ctx) => ctx.reply("Welcome WEB APP!"));
+
+bot.launch();
--- bot/bot.ts
+++
@@ -1,5 +0,0 @@
-import { Telegraf } from "telegraf/typings/telegraf";
-const TOKEN = "6422144117:AAFyAMiQym1MR2TBVBhJkobbCpZwOwlQqHQ";
-const bot = new Telegraf(TOKEN);
-bot.start((ctx) => ctx.reply("Welcome WEB APP!"));
-bot.launch();
