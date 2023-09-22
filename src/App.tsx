0997f9c0708eac43593d9c88bb966cb8755a9c3e
aydosjs <aji3385@gmail.com>
2023-09-22 18:28:26 +0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { useEffect } from 'react';
 import './App.css'
 import Height from './containers/Height'
 import MainLayout from './containers/MainLayout'
 
+const tg = window.Telegram.WebApp;
+
 function App() {
+
+  useEffect(() => {
+    tg.ready()
+  }, [])
+
   return (
     <MainLayout>
       <Height />
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { WebAppProvider } from '@vkruglikov/react-telegram-web-app';
 import './index.css'
-import '../bot/bot.js'
+
+
+import { Telegram } from "@twa-dev/types"
+
+declare global {
+  interface Window {
+    Telegram: Telegram;
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
